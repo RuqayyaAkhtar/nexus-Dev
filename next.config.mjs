6e63b94352cc4b9078d2795c993d058fd54021f4
RuqayyaAkhtar <112484548+RuqayyaAkhtar@users.noreply.github.com>
2025-04-20 16:03:12 +0500

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',   
-    images: {
-      unoptimized: true, 
-    },
-    distDir: 'out',   
-  };
-  
-  export default nextConfig;
-  
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  distDir: 'out',
+};
+
+export default nextConfig;
+
